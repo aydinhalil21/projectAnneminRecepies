68f6caca8ea9283eb9feda327c4a91aa7dbb1a56
aydinhalil21 <aydinhalil21@gmail.com>
2023-10-05 17:15:07 +0200

    thursdays chill

--- projectAnne/src/App.jsx
+++ projectAnne/src/App.jsx
@@ -1,7 +1,15 @@
 import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import addPage from './pages/addPage'
+import allRecepiesPage from './pages/allRecepiesPage'
+/* import feedPage from './pages/feedPage'
+import profilePage from './pages/profilePage' */
+import recepiePage from './pages/recepiePage'
+import updatePage from './pages/updatePage'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,25 +17,14 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Routes>
+        <Route path="/" element={allRecepiesPage} />
+        <Route path="/recepie-page/:recepieId" element={recepiePage} />
+        <Route path="/add-recepie" element={addPage} />
+        <Route path="/edit-recepie" element={updatePage} />
+        </Routes>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
